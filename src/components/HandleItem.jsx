57dbdc1e83a459188b27515867a43ffe593381b6
KUM <ancolor@knou.ac.kr>
2024-07-22 16:14:41 +0900

    주문처리 레이아웃 수정

--- src/components/HandleItem.jsx
+++ src/components/HandleItem.jsx
@@ -6,12 +6,12 @@ export default function HandleItem({order}) {
   const {orderName, paymentId, userInfo} = order;
   return (
     <li>
-      <div 
-      className='bg-brand text-white line-clamp-1 text-sm rounded-md w-full p-1 cursor-pointer my-1'
+      <p 
+      className='bg-brand text-white line-clamp-1 text-sm rounded-md w-full p-1 cursor-pointer my-1 hover:brightness-110'
       onClick={() => navigate(`/order/handle/${paymentId}`, {state: {order, userInfo}})}
       >
-        {orderName}
-      </div>
+        <span className='bg-white text-brand rounded-md px-1 mr-2'>{paymentId.slice(-4)}</span>{orderName}
+      </p>
     </li>
   );
 }
--- src/pages/OrderHandle.jsx
+++ src/pages/OrderHandle.jsx
@@ -9,9 +9,14 @@ export default function OrderHandle() {
   return (
     <section className='p-8 pt-4'>
       <p className='text-2xl text-center fond-bold pb-4 mb-4 border-b border-gray-300'>주문처리</p>
-      <div className='flex flex-col sm:flex-row justify-between gap-4 h-96 text-center'>
+      <div className='flex flex-col items-center py-8 gap-2'>
+        <p className='text-center text-xl'>담당자는 주문건의 상세내역에서 배송목록을 확인 후 해당주문을 다음단계로 처리해주세요!</p>
+        <p className='text-center text-xl'>해당 주문건을 클릭하면 상세내역페이지로 이동합니다.</p>
+        <p className='text-center text-gray-500'>(결제완료 → 주문확인 → 배송중 → 배송완료)</p>
+      </div>
+      <div className='flex flex-col sm:flex-row justify-between gap-4 h-96'>
         <div className='bg-gray-100 rounded-lg p-3 basis-1/4'>
-          <p className='text-lg'>결제완료</p>
+          <p className='text-lg text-center'>결제완료</p>
           <ul className='overflow-scroll'>
             {allOrders
             .filter(order => order.status === '결제완료')
@@ -19,7 +24,7 @@ export default function OrderHandle() {
           </ul>
         </div>
         <div className='bg-gray-100 rounded-lg p-3 basis-1/4'>
-          <p className='text-lg'>주문확인</p>
+          <p className='text-lg text-center'>주문확인</p>
           <ul className='overflow-scroll'>
             {allOrders
             .filter(order => order.status === '주문확인')
@@ -27,7 +32,7 @@ export default function OrderHandle() {
           </ul>
         </div>
         <div className='bg-gray-100 rounded-lg p-3 w-full basis-1/4'>
-          <p className='text-lg'>배송중</p>
+          <p className='text-lg text-center'>배송중</p>
           <ul className='overflow-scroll'>
             {allOrders
             .filter(order => order.status === '배송중')
@@ -35,7 +40,7 @@ export default function OrderHandle() {
           </ul>
         </div>
         <div className='bg-gray-100 rounded-lg p-3 w-full basis-1/4'>
-          <p className='text-lg'>배송완료</p>
+          <p className='text-lg text-center'>배송완료</p>
           <ul className='overflow-scroll'>
             {allOrders
             .filter(order => order.status === '배송완료')
